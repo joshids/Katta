1d5fc5fe25856c0b9fed9039ed271950ccc26f59
Darshan JOSHI <joshids.work@gmail.com>
2024-09-15 17:17:43 +0200

    SpecFlow setup works

---
+++ SpecFlow.Calculator.Specs/Drivers/Driver.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace SpecFlow.Calculator.Specs.Drivers
+{
+    public class Driver
+    {
+
+    }
+}
---
+++ SpecFlow.Calculator.Specs/Hooks/Hook.cs
@@ -0,0 +1,11 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlow.Calculator.Specs.Hooks
+{
+    [Binding]
+    public class Hooks
+    {
+        
+    }
+}
